f2347d4cacd8ddc984302504d4dfad7e2de66162
Ahmad Seyar Miakhil <seyarahmad327@gmail.com>
2025-12-17 13:36:49 +0430

    initla commit

--- src/App.tsx
+++ src/App.tsx
@@ -11,11 +11,13 @@ import ClearLocalStorage from './Pages/singn-up/local';
 import DashboardHomePage from './Pages/Dashboard/DashboardHomePage';
 import DashboardCoursesPage from './Pages/Dashboard/DashboardCoursesPage';
 import Footer from './Components/Footer';
+import SingleCoursePage from './Pages/SingleCourse/SingleCourse';
 
 function App() {
   return (
     <div className="min-h-screen overflow-x-hidden">
       <Routes>
+        <Route path="/singlecourse" element={<SingleCoursePage />} />
         <Route path="/login" element={<Login />} />
         <Route path="/signup" element={<Signup />} />
         <Route path="/onboarding" element={<OnBoardingPage />} />
